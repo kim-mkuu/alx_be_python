c544d8599a98afbff28d3c6f9272ae3aba083792
kim-mkuu <kimukuphilip10@gmail.com>
2025-05-25 22:42:24 +0300

    Initial commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+#Basic Python operands
+
+#variables definition
+number1 = 10
+number2 =  5
+
+# This script performs basic arithmetic operations: addition, subtraction, and multiplication.
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+#print the results in human readable format
+print(f"Addition of {number1} and {number2} is: {addition}")
+print(f"Subtraction of {number1} and {number2} is: {subtraction}")
+print(f"Multiplication of {number1} and {number2} is: {multiplication}")
