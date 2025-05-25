b6641de20449fbab237fcf1f3ae98778681846c2
kim-mkuu <kimukuphilip10@gmail.com>
2025-05-26 00:58:01 +0300

    Simple Intrest file addition

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,11 +1,11 @@
 # A script to calculate simple interest
 
-P = Principal_Amount = 1000  # P represents the Principal Amount
-R = Annual_Interest_Rate = 0.05  # R represents the Annual Interest Rate
-T = Investment_Duration = 3  # T represents the Time in years
+Principal = 1000  # P represents the Principal Amount
+Rate = 0.05  # R represents the Annual Interest Rate
+Time = 3  # T represents the Time in years
 
 # Calculate simple interest using I = P * R * T
-Interest_Earned = I = P * R * T  # I represents the Interest Earned
+Interest_Earned = I = Principal * Rate * Time  # I represents the Interest Earned
 
 # Print the result in a formatted output
 print(f"The simple interest is: {Interest_Earned}")
